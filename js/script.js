6034d60bca07363016842b99b3449a9ee130d1bf
Gabriel Rangel <glrangel25@gmail.com>
2017-05-23 15:24:45 -0700

    Intial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function loginLoad(){
+	document.getElementById('title-container').style.opacity = "1";
+	console.log("yay!");
+}
+function assignmentLoad(){
+	document.getElementById('assign-container').style.transform = "translateY(0px)";
+	document.getElementById('title-container').style.transform = "translateY(0px)";
+	console.log("yay!");
+}
+function assignmentTransition(){
+
+	document.getElementById('assign-container').style.transform = "translateY(700px)";
+	document.getElementById('title-container').style.transform = "translateY(700px)";
+	
+	setTimeout(function(){
+		window.location.href = "energyvampire.html";
+	}, 700);
+
+}
+function vampireLoad(){
+		document.getElementById('title-container').style.opacity = "1";
+		document.getElementById('banner-container').style.opacity = "1";
+
+
+}
