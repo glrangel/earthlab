37f59d21b56678152554131578185e31412abc05
Gabriel Rangel <glrangel25@gmail.com>
2017-05-29 15:52:22 -0700

    Styled energy page. Saved number of items into array.

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -6,7 +6,7 @@ var data = [
     value: 300,
     color:"#F7464A",
     highlight: "#FF5A5E",
-    label: "Red"
+    label: "TV"
 },
 {
     value: 50,
@@ -24,3 +24,18 @@ var data = [
 // Get the context of the canvas element we want to select
 var ctx = document.getElementById("myChart").getContext('2d');
 var myDoughnutChart = new Chart(ctx).Doughnut(data,option);
+
+
+// value_array[0] = TV
+// value_array[1] = Tablet
+// value_array[2] = Video Game Console
+// value_array[3] = Lamp
+
+// value_array[4] = Toaster
+// value_array[5] = Microwave
+// value_array[6] = Coffee Maker
+
+// value_array[7] = Laptop
+// value_array[8] = Cellphone Charger
+// value_array[9] = Computer (Desktop)
+// value_array[10] = Printer
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+
+// Animation functions
 function loginLoad(){
 	document.getElementById('title-container').style.opacity = "1";
 	console.log("yay!");
@@ -8,8 +10,8 @@ function assignmentLoad(){
 	console.log("yay!");
 }
 function assignmentTransition(){
-	document.getElementById('assign-container').style.transform = "translateY(700px)";
-	document.getElementById('title-container').style.transform = "translateY(700px)";
+	document.getElementById('assign-container').style.transform = "translateY(100vh)";
+	document.getElementById('title-container').style.transform = "translateY(100vh)";
 	
 	setTimeout(function(){
 		window.location.href = "energyvampire.html";
@@ -18,9 +20,43 @@ function assignmentTransition(){
 }
 function vampireLoad(){
 		document.getElementById('title-container').style.opacity = "1";
-		document.getElementById('banner-container').style.opacity = "1";
 
 }
 function showGraphs(){
 	$('#myChart').show();
+}
+
+
+
+//Puts values chosen into an array listed below
+
+var value_array = [];
+
+// value_array[0] = TV
+// value_array[1] = Tablet
+// value_array[2] = Video Game Console
+// value_array[3] = Lamp
+
+// value_array[4] = Toaster
+// value_array[5] = Microwave
+// value_array[6] = Coffee Maker
+
+// value_array[7] = Laptop
+// value_array[8] = Cellphone Charger
+// value_array[9] = Computer (Desktop)
+// value_array[10] = Printer
+
+$(document).ready(function () {
+	$('.form-control').change(function(){
+	    value_array = $('.form-control').find('option:selected').map(function(){return $(this).val();});
+	});
+});
+
+//scroll to results page
+function resultsPage(){
+	console.log("clicked");
+	$('#results-container').css('display', 'inherit');
+	$('body').animate({
+        scrollTop: $("#results-container").offset().top
+    }, 1000);
 }
